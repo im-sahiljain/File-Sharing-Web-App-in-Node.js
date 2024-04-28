5da88f096be173693f5b96bb8e8273ece6b18333
im-sahiljain <mr.sahiljain14@gmail.com>
2024-04-28 22:46:34 +0530

    Initial commit

---
+++ models/File.js
@@ -0,0 +1,22 @@
+// model that represents our file database object
+
+const mongoose = require("mongoose")
+
+const File = new mongoose.Schema({
+    path:{
+        type:String,
+        required: true
+    },
+    originalName:{
+        type:String,
+        required: true
+    },
+    password:String,
+    downloadCount:{
+        type: Number,
+        required:true,
+        default:0
+    }
+})
+
+module.exports = mongoose.model("File", File) //("name of table in db", Schema name)
